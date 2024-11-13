c740cc982e9cecf1bba083b4bf0ce86ee1c3eb94
GustavoDeLim4 <127445721+GustavoDeLim4@users.noreply.github.com>
2024-11-13 19:17:31 -0300

    Usuario.java

--- app/src/main/java/com/example/taskzone/model/Usuario.java
+++ app/src/main/java/com/example/taskzone/model/Usuario.java
@@ -7,7 +7,6 @@ public class Usuario {
     private String email;
     private String senha;
 
-    // Getters e Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
